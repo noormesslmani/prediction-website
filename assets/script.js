dea7344920142390a26af6b20266e948a3879295
noormesslmani <noor.messlmani@gmail.com>
2022-09-10 05:54:32 +0300

    Fixe a bug in js which had to do with empty input

--- assets/script.js
+++ assets/script.js
@@ -12,48 +12,55 @@ button.addEventListener('click',predictInformation)
 
 //function that generates predictions
 function predictInformation(){
-
-    //initialize text contents
-    gen.textContent='gender: '
-    age.textContent='age: '
-    nat.textContent='nationalities: '
-
-    //predict gender and concatenate it to gen text content
     fname= inputName.value
-    url='https://api.genderize.io/?name='+ fname
-    fetch(url)
-        .then(res => res.json())
-        .then(data => {
-            gen.textContent += data.gender       
-    })
-    
 
-    //predict age and concatenate it to age text content
-    url='https://api.agify.io/?name='+ fname
-    fetch(url)
-        .then(res => res.json())
-        .then(data => {
-            age.textContent += data.age       
-    })
-    
-    //predict possible countries and concatenate them to countries text content
-    let countries=''
-    url='https://api.nationalize.io/?name='+ fname
-    fetch(url)
-        .then(res => res.json())
-        .then(data => {
-            //loop over all possible countries and concatenate them to countries
-            for (let i=0;i<data.country.length; i++){
-                countries+= data.country[i]["country_id"]
-                if (i<data.country.length-1){
-                    countries+=", "
-                }
-            }  
-        nat.textContent += countries
-    })
+    //check if the user entered his name
+    if (fname){
+        //initialize predictions text contents
+        gen.textContent='gender: '
+        age.textContent='age: '
+        nat.textContent='nationalities: '
+
+        //predict gender and concatenate it to gen text content
+        url='https://api.genderize.io/?name='+ fname
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                gen.textContent += data.gender       
+        })
+        
+        //predict age and concatenate it to age text content
+        url='https://api.agify.io/?name='+ fname
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                age.textContent += data.age       
+        })
+        
+        //predict possible countries and concatenate them to countries text content
+        let countries=''
+        url='https://api.nationalize.io/?name='+ fname
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                //loop over all possible countries and concatenate them to countries
+                for (let i=0;i<data.country.length; i++){
+                    countries+= data.country[i]["country_id"]
+                    if (i<data.country.length-1){
+                        countries+=", "
+                    }
+                }  
+            nat.textContent += countries
+        })
+    }
+
+    //Otherwise predictions would be undefined
+    else{
+        gen.textContent= "Undefined, please enter your name"
+    }
 }
 
-//A function to display dog image
+//A function to display dog image onload
 //an image tag is added to 'dog' div with a class=dog
 function displayDog(){
     fetch('https://dog.ceo/api/breeds/image/random')

e2ed2faf58ab819b005e9f6f3f9f0e3616f2b49d
noormesslmani <noor.messlmani@gmail.com>
2022-09-11 21:40:46 +0300

    Added a sign oout button and an invalid login data message

--- assets/script.js
+++ assets/script.js
@@ -4,28 +4,35 @@ const inputName = document.getElementById('input-name')
 const gen = document.getElementById('gender')
 const age = document.getElementById('age')
 const nat = document.getElementById('nationalities')
-const button = document.getElementById('button')
+const submitButton = document.getElementById('submit-button')
 const name1 = document.getElementById('name')
 const pw = document.getElementById('pw')
 const register= document.getElementById("rgstr_btn")
-const login= document.getElementById("login_btn")
-const signUp=document.getElementById("sign-up")
+const signInButton= document.getElementById("signin-button")
+const signMenu=document.getElementById("sign-menu")
 const enterName=document.getElementById("enter-name")
+const signOutButton= document.getElementById("signout-button")
+const logInForm= document.getElementById("login-form")
 let fname
 let url
+//to be displayed when invalid username/password is entered
+let para = document.createElement("p")
+const node = document.createTextNode("Invalid username/password")
 
+//sign-in/registration part
 
-
-register.addEventListener('click',store)
-function store() {
+//store entered username and password in Local storage upon clicking on register button
+register.addEventListener('click',storeData)
+function storeData() {
     localStorage.setItem('name', name1.value)
     localStorage.setItem('pw', pw.value)
+    window.location.reload()
 }
 
-// check if stored data from register-form is equal to entered data in the login-form
-login.addEventListener('click',check)
-function check() {
-
+//check if stored data from register-form matches the entered data in the login-form
+//add an event listener upon click to signin button
+signInButton.addEventListener('click',checkData)
+function checkData() {
     // stored data from the register-form
     var storedName = localStorage.getItem('name')
     var storedPw = localStorage.getItem('pw')
@@ -33,19 +40,35 @@ function check() {
     // entered data from the login-form
     var userName = document.getElementById('userName')
     var userPw = document.getElementById('userPw')
+
+    
+    //if correct data is entered display the enter your name menu
     if(userName.value == storedName && userPw.value == storedPw){
-        signUp.style.display= "none"
-        enterName.style.display=''
+        signMenu.style.display= "none" //hide sign in menu
+        enterName.style.display='' //display ennter name menu
+    }
+    //otherwise display an Invalid username/password message
+    else{
+        para.appendChild(node)
+        if (logInForm.querySelector('.para') == null) {
+            document.getElementById("login-form").appendChild(para)   
+        }
     }
 }
 
 
+//logout click and display signin menu on the screen
+signOutButton.addEventListener('click',logOut)
+function logOut(){
+    signMenu.style.display= '' //display signin menu
+    enterName.style.display='none' //hide enter name menu
+}
 
 
+//predictions part
 
 //add an event listener to the submit bottun on click
-button.addEventListener('click',predictInformation)
-
+submitButton.addEventListener('click',predictInformation)
 //function that generates predictions
 function predictInformation(){
     fname= inputName.value
@@ -120,10 +143,12 @@ function predictInformation(){
     }
 }
 
+
 //A function to display dog image onload
 //an image tag is added to 'dog' div with a class=dog
 function displayDog(){
-    enterName.style.display="none"
+    enterName.style.display='none' //hide enter name menu
+    //display the dog image
     fetch('https://dog.ceo/api/breeds/image/random')
         .then(res => res.json())
         .then(data => {
